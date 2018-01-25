a7f1950129f2d8da60f505197a328b80b4b027d1
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-25 10:12:22 -0500

    Fixed a type in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.josi(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 const server = app.listen(port, () => {
