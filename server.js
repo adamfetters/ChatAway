7d8496018e4b6e374ed867b5c2fdf931cf0bd52e
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-25 09:50:36 -0500

    Changed port settings to adjust for Heroku

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const socket = require('socket.io');
 const cors = require('cors');
 const app = express();
 
-const server = app.listen(5000, () => {
-  console.log('Server listening on port 5000');
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
 app.use(cors());
